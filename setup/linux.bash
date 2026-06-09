45b646dcdce2741e53670e0d8a803a598a81bd02
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-06-09 19:32:06 -0400

    Update linux.bash

--- setup/linux.bash
+++ setup/linux.bash
@@ -38,16 +38,6 @@ sudo apt-get install -y libgl-dev libgl1-mesa-dev libasound2-dev
 sudo apt-get install -y libdrm-dev libgbm-dev mesa-common-dev libegl1-mesa-dev libgles2-mesa-dev
 sudo apt-get install -y libinput-dev libudev-dev
 
-# Setup i386
-log_info "Setting up i386 architecture..."
-sudo dpkg --add-architecture i386
-sudo apt update
-
-# Install 32-bit dependencies
-log_info "Installing 32-bit dependencies..."
-sudo apt-get install -y libgl-dev:i386 libgl1-mesa-dev:i386 libglu1-mesa-dev:i386
-sudo apt-get install -y libdrm-dev:i386 libgbm-dev:i386 mesa-common-dev:i386 libegl1-mesa-dev:i386 libgles2-mesa-dev:i386
-
 # Fix DRM headers
 log_info "Fixing DRM headers..."
 sudo sed -i 's|<drm_mode.h>|<libdrm/drm_mode.h>|' /usr/include/xf86drmMode.h || true
